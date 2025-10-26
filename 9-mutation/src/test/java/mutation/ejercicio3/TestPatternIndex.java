f480fe836f3773c8bbc19dc37704311738e73a11
francisco <francisco_natale@hotmail.com>
2025-10-26 16:17:44 -0400

    add test of exercise 1

---
+++ 9-mutation/src/test/java/mutation/ejercicio3/TestPatternIndex.java
@@ -0,0 +1,47 @@
+package mutation.ejercicio3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestPatternIndex {
+
+    @Test
+    void testQueNoEsPatronNiCoincidenEnElPrimerCaracter() {
+        String subject = "abracadabra";
+        String pattern = "ba";
+        int result = PatternIndex.patternIndex(subject, pattern);
+        assertEquals(-1, result);
+    }
+    @Test
+    void testQueEsPatronYCoincidenEnElPrimerCaracter() {
+        String subject = "abracadabra";
+        String pattern = "abra";
+        int result = PatternIndex.patternIndex(subject, pattern);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void testQueEsUnPatronPeroNoEjecutaElFor(){
+        String subject = "a";
+        String pattern = "a";
+        int result = PatternIndex.patternIndex(subject, pattern);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void testQueEsUnPatronPeroNoEmpiezaEnElPrimerCaracter() {
+        String subject = "patapin";
+        String pattern = "atapin";
+        int result = PatternIndex.patternIndex(subject, pattern);
+        assertEquals(1, result);
+    }
+
+    @Test
+    void testQueNoEsUnPatronNiSeConsultaUnPatron(){
+        String subject = "";
+        String pattern = "a";
+        int result = PatternIndex.patternIndex(subject, pattern);
+        assertEquals(-1, result);
+
+    }
+}
