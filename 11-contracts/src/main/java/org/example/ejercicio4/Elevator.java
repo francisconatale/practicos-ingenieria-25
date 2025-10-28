99a3ae2cdde2e99e7f50b685a991043158ce75fe
francisco <francisco_natale@hotmail.com>
2025-10-28 19:44:40 -0300

    add exercise 4 and pending tests

---
+++ 11-contracts/src/main/java/org/example/ejercicio4/Elevator.java
@@ -0,0 +1,71 @@
+package org.example.ejercicio4;
+
+    enum ElevatorState { IDLE, MOVING_UP, MOVING_DOWN, OUT_OF_SERVICE }
+
+    /**
+     * - currentFloor >= 1 && currentFloor <= maxFloor
+     */
+    public class Elevator {
+        private int currentFloor;
+        private int maxFloor;
+        private ElevatorState state;
+
+        public Elevator(int maxFloor) {
+            this.maxFloor = maxFloor;
+            this.state = ElevatorState.IDLE;
+            this.currentFloor = 0;
+        }
+
+
+        public void callToFloor(int floor) {
+            assert (floor >= 1 && floor <= maxFloor) : "Invalid call to floor";
+            assert (!isOutOfService()) : "The elevator is out of service";
+            if (floor > currentFloor) movingUp(floor);
+            if(floor < currentFloor) movingDown(floor);
+            assert repOK();
+        }
+
+        private void movingUp(int expectedFloor) {
+            try {
+                state = ElevatorState.MOVING_UP;
+                while (currentFloor <= expectedFloor) {
+                    currentFloor++;
+                }
+                state = ElevatorState.IDLE;
+            } catch (IllegalStateException e) { // never exception in code, is only a simulation of real scenario
+                state = ElevatorState.OUT_OF_SERVICE;
+            }
+        }
+
+        private void movingDown(int expectedFloor) {
+            try {
+                state = ElevatorState.MOVING_DOWN;
+                while (currentFloor >= expectedFloor) {
+                    currentFloor--;
+                }
+                state = ElevatorState.IDLE;
+            } catch (IllegalStateException e) { // never exception in code, is only a simulation of real scenario
+                state = ElevatorState.OUT_OF_SERVICE;
+            }
+        }
+
+        private boolean isOutOfService() {
+            return state == ElevatorState.OUT_OF_SERVICE;
+        }
+
+        private boolean elevatorIsMoving() {
+            return state == ElevatorState.MOVING_UP || state == ElevatorState.MOVING_DOWN;
+        }
+
+        public void stop(){
+            assert (!isOutOfService()) : "The elevator is out of service";
+            assert (elevatorIsMoving()) : "The elevator is moving";
+            state = ElevatorState.IDLE;
+        }
+
+        public boolean repOK(){
+            return (currentFloor >= 0 && currentFloor <= maxFloor);
+        }
+
+    }
+
